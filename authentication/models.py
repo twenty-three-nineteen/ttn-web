c434971572782fec62d97749011c79f9020baee2
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2020-11-06 00:03:58 +0330

    remove extra fields and add user manager

--- authentication/models.py
+++ authentication/models.py
@@ -1,14 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("User Must Have Email Address!!")
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username)
+        user.set_password(password)
+        user.save()
+        return user
+
 
 class User(AbstractUser):
     email = models.EmailField(verbose_name='email', max_length=255, unique=True)
-    phone = models.CharField(null=True, max_length=255)
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone']
+    username = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', ]
+
+    def get_email(self):
+        return self.email
 
     def get_username(self):
-        return self.email
+        return self.username
 
     class Meta:
         db_table = "users"
