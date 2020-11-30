aa180b2553f50cf1c0435558c419329e3493dd3a
AmirH Ahmadi <amirh.khali@gmail.com>
2020-11-30 20:31:56 +0330

    nothing

--- account/admin.py
+++ account/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
 admin.site.register(models.UserProfile)
 admin.site.register(models.Avatar)
 admin.site.register(models.RequestModel)
 admin.site.register(models.OpeningMessage)
+admin.site.register(models.Interest)
+admin.site.register(models.User)
