860a8af7fd45e0c72cddc27037d1fb5ffbc0a243
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2020-12-20 13:43:57 +0330

    change url get pages

--- account/urls.py
+++ account/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('userprofile/<slug:username>', user_profile),
     path('opening_messages/', opening_message_create),
-    path('opening_messages/<int:page>', opening_message_list),
+    path('opening_messages/page/<int:page>', opening_message_list),
     path('opening_messages/<int:pk>', opening_message_detail),
     path('explore/suggested_opening_message/', explore),
     path('send_chat_request/', request_message),
